3727e0231e64f65935cf74cd16709821bf9473f6
Margarita Mayer <rita.mayer2001@mail.ru>
2023-04-22 11:58:56 +0300

    Move folder shop to frontend

--- frontend/shop/src/app/app.module.ts
+++ frontend/shop/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { ShopCartComponent } from './shop/shop-cart/shop-cart.component';
+import { ShopItemComponent } from './shop/shop-item/shop-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    ShopCartComponent,
+    ShopItemComponent,
   ],
   imports: [
     BrowserModule
---
+++ frontend/shop/src/app/shop/shop-item/shop-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-item',
+  templateUrl: './shop-item.component.html',
+  styleUrls: ['./shop-item.component.less']
+})
+export class ShopItemComponent {
+
+}
